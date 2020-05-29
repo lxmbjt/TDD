e602a9a30b1c6cf9557302e5ca76ba12b7eeea3b
lxmbjt <1192770932@qq.com>
2020-05-29 13:46:28 +0800

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,35 +6,19 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 MAX_WAIT=10
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
-	def test_layout_and_styling(self):
-		#Edith goes to the home page
-		self.browser.get(self.live_server_url)
-		self.browser.set_window_size(1024,768)
-		
-		#She notice the input box is nicely centered
-		inputbox=self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x']+inputbox.size['width']/2,
-			512,
-			delta=10
-			)
-		#She starts a new list and sees the input is nicely centered there too
-		inputbox.send_keys('testing')
-		inputbox.send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1:testing')
-		inputbox=self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x']+inputbox.size['width']/2,
-			512,
-			delta=10
-			)
+	
 	def setUp(self):
 		self.browser=webdriver.Firefox()
+		#staging_server=os.environ.get('STAGING_SERVER')
+		staging_server=os.environ.get('49.234.113.122');
+		if staging_server:
+			self.live_server_url='http://'+staging_server
 
 	def tearDown(self):
 		self.browser.quit()
@@ -85,7 +69,28 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
 		#Satisfied,she goes to sleep
 		
-
+	def test_layout_and_styling(self):
+		#Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+		
+		#She notice the input box is nicely centered
+		inputbox=self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x']+inputbox.size['width']/2,
+			512,
+			delta=10
+			)
+		#She starts a new list and sees the input is nicely centered there too
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1:testing')
+		inputbox=self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x']+inputbox.size['width']/2,
+			512,
+			delta=10
+			)
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		#Edith starts a new to-do list
 		self.browser.get(self.live_server_url)
